aee7d2fdd0ad21f1aa3bba94034bc755f08991d9
KevinBett <kevinbett458@gmail.com>
2023-03-15 11:02:11 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function divide(num1 = '2000', num2 = '100'){
+  return divide = num1/num2;
+}
+
+const square = function(num){
+  return num * num;
+}
+
+const add = function(num1, num2){
+  return num1 + num2;
+}
